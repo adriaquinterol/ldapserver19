2851e0e079a65e3e6b432e06102ad36d20f6bd69
AdriaQuintero <adria.quintero@gmail.com>
2019-10-28 09:29:46 +0100

    directori i imatge practica creats

--- ldapserver19:practica/install.sh
+++ ldapserver19:practica/install.sh
@@ -6,6 +6,7 @@ rm -rf /var/lib/ldap/*
 cp /opt/docker/DB_CONFIG /var/lib/ldap/.
 slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d
 slapadd -F /etc/openldap/slapd.d -l /opt/docker/edt.org.ldif
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/alumnes_matricula.ldif
 chown -R ldap.ldap /etc/openldap/slapd.d
 chown -R ldap.ldap /var/lib/ldap
 cp /opt/docker/ldap.conf /etc/openldap/.
